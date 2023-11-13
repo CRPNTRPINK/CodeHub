5fcdbf75593f042b5d6319f599646c754d1e14b1
Veiga <agiev@vtb.ru>
2023-11-13 04:30:59 +0300

    add tests

--- settings.py
+++ settings.py
@@ -8,3 +8,8 @@ REAL_DATABASE_URL = os.getenv(
     "REAL_DATABASE_URL",
     "postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"
 )
+
+TEST_DATABASE_URL = os.getenv(
+    "TEST_DATABASE_URL",
+    "postgresql+asyncpg://postgres:postgres@localhost:5433/postgres"
+)
